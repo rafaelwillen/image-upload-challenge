79d6c186b57149dfc0e016a6ed995b0cf3089e4a
Rafael Willen <rafaelpadre20@gmail.com>
2023-02-11 18:41:08 +0100

    Add drag and drop functionality

--- client/src/components/DragAndDrop.tsx
+++ client/src/components/DragAndDrop.tsx
@@ -1,8 +1,34 @@
+import { DragEvent } from "react";
 import ImageSVG from "../assets/images/image.svg";
 
-const DragAndDrop = () => {
+type Props = {
+  onFileDrop?: (file: File) => void;
+};
+
+const DragAndDrop = ({ onFileDrop }: Props) => {
+  function handleDragEnter(e: DragEvent) {
+    e.stopPropagation();
+    e.preventDefault();
+  }
+
+  function handleDragOver(e: DragEvent) {
+    e.stopPropagation();
+    e.preventDefault();
+  }
+
+  function handleDragDrop(e: DragEvent) {
+    e.preventDefault();
+    const file = e.dataTransfer.files[0];
+    if (onFileDrop) onFileDrop(file);
+  }
+
   return (
-    <div className="py-9 px-28 bg-[#F6F8FB] border border-dashed border-[#97BEF4] rounded-xl mt-8 flex flex-col items-center">
+    <div
+      className="py-9 px-28 bg-[#F6F8FB] border border-dashed border-[#97BEF4] rounded-xl mt-8 flex flex-col items-center"
+      onDragEnter={handleDragEnter}
+      onDragOver={handleDragOver}
+      onDrop={handleDragDrop}
+    >
       <img width={114} height={88} className="w-28 h-20" src={ImageSVG} />
       <p className="text-xs text-[#BDBDBD] mt-9 select-none">
         Drag & Drop your image here
