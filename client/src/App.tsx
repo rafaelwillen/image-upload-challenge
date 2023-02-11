b1e4f6d3de4864d4a7c064d3226f65b2dd60a3e9
Rafael Willen <rafaelpadre20@gmail.com>
2023-02-11 17:32:52 +0100

    Add custom font

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="container">
+    <div className="container font-main font-medium">
       <h1 className="text-lg">Hello World</h1>
     </div>
   );
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{ts,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
+    fontFamily: {
+      main: "Poppins, sans-serif",
+    },
+    fontWeight: {
+      medium: "500",
+      bold: "700",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
